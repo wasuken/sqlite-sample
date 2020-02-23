ab1ec5605293cb12162cdef9e61ba9d9ce606373
wasuken <wevorence@gmail.com>
2020-02-23 15:28:20 +0900

    bug fix.

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -8,9 +8,9 @@ open MyDB.Connection
 [<EntryPoint>]
 let main argv =
   let curDir = Directory.GetCurrentDirectory() + "/"
-  let con = mkSharedF
+  let con = (fun unit -> mkSharedDBNameF "prod.sqlite")
   let result = match (File.Exists(curDir + "prod.sqlite")) with
-    | false -> Some (Queries.InitializeTable con |> Async.RunSynchronously)
+    | false -> Some (Queries.InitializeTable con |> Async.RunSynchronously) |> ignore
     | true -> None
   let person = Queries.Person.TryFindByName con "hoge" |> Async.RunSynchronously
   printfn "%s" person.Value.Name
