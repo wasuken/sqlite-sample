1634e8444aecd41c2c29bad7278f6973d9f12933
wasuken <wevorence@gmail.com>
2020-02-23 15:49:10 +0900

    success test.

--- test/MyDBTest/UnitTest1.fs
+++ test/MyDBTest/UnitTest1.fs
@@ -2,6 +2,7 @@ module MyDBTest
 
 open NUnit.Framework
 open MyDB
+open System.IO
 
 [<SetUp>]
 let Setup () =
@@ -9,10 +10,12 @@ let Setup () =
 
 [<Test>]
 let Test1 () =
-    let con = Connection.mkMemoryShared
-    con.Open()
-    let con2 = (fun unit -> Connection.mkMemorySharedF(con))
-    Queries.InitializeTable con2 |> Async.RunSynchronously |> ignore
-    let person = Queries.Person.TryFindByName  con2 "hoge" |> Async.RunSynchronously
-    Assert.True(person.Value.Name.Equals("hoge"))
-    con.Close()
+  let curDir = Directory.GetCurrentDirectory() + "/"
+  match (File.Exists(curDir + "test.sqlite")) with
+    | false -> Some (File.Delete(curDir + "test.sqlite"))
+    | true -> None
+  let con = (fun unit -> Connection.mkSharedDBNameF "test.sqlite")
+  Queries.InitializeTable con |> Async.RunSynchronously |> ignore
+  let person = Queries.Person.TryFindByName  con "hoge" |> Async.RunSynchronously
+  Assert.True(person.Value.Name.Equals("hoge"))
+  File.Delete(curDir + "test.sqlite")
