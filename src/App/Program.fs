720ce5311fe87377988d3b7401cb501a8a2038e8
wasuken <wevorence@gmail.com>
2020-02-28 18:55:22 +0900

    remove ignore.

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -10,7 +10,7 @@ let main argv =
   let curDir = Directory.GetCurrentDirectory() + "/"
   let con = (fun unit -> mkSharedDBNameF "prod.sqlite")
   let result = match (File.Exists(curDir + "prod.sqlite")) with
-    | false -> Some (Queries.InitializeTable con |> Async.RunSynchronously) |> ignore
+    | false -> Some (Queries.InitializeTable con |> Async.RunSynchronously)
     | true -> None
   let person = Queries.Person.TryFindByName con "hoge" |> Async.RunSynchronously
   printfn "%s" person.Value.Name
